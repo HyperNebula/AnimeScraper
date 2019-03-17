f6c003ab29e0fcfcb02d34e0db85f9f5660a8746
TheNexus <36902158+The-Canadians-Friend@users.noreply.github.com>
2019-03-16 23:30:48 -0400

    Adds info into table

--- main.py
+++ main.py
@@ -16,11 +16,24 @@ html_str = """<!DOCTYPE html>
         <center>
 
             <h1>Anime Info</h1>
+            
+            <table style="width:100%">
+                
+                <tr>
+                    
+                    <th><u>Image</u></th>
+                    <th><u>Title</u></th>
+                    <th><u>English Title</u></th>
+                    <th><u>Description</u></th>
+                    <th><u>Rating</u></th>
+                    <th><u>Genres</u></th>
+                    
+                </tr>
 """
 f.write(html_str)
 f.close()
 
-animes = ["Samurai Champloo", "Sword Art Online"]
+animes = ["Samurai Champloo", "Toradora"]
 
 for show in animes:
 
@@ -41,30 +54,35 @@ for show in animes:
 
     name = str(soup.find("span", itemprop="name"))
     image_url = str(soup.find("img", itemprop="image"))
-    description = str(repr(soup.find("span", itemprop="description")))
+    description = str(soup.find("span", itemprop="description")).replace("[Written by MAL Rewrite]", "")
     score = str(soup.find("span", itemprop="ratingValue"))
 
     rating = str(soup.find(text="Rating:").parent.parent).replace("Rating:", "")
     eng_name = str(soup.find(text="English:").parent.parent).replace("English:", "")
     duration = str(soup.find(text="Duration:").parent.parent).replace("Duration:", "")
-    genres = str(soup.find(text="Genres:").parent.parent).replace("Genres:", "")
+    genres = str(soup.find(text="Genres:").parent.parent.text).replace("Genres:", "")
     status = str(soup.find(text="Status:").parent.parent).replace("Status:", "")
     episode_num = str(soup.find(text="Episodes:").parent.parent).replace("Episodes:", "")
     premiere_date = str(soup.find(text="Premiered:").parent.parent).replace("Premiered:", "")
 
     anime_info = [name, eng_name, description, score, rating, duration, episode_num, genres, status, premiere_date, image_url]
 
+    fmt = "<th>{}</th>"
     with open('main.html', 'a') as fd:
-        fd.write(image_url)
-        fd.write(name)
-        fd.write(eng_name)
-        fd.write(description)
-        fd.write(rating)
-        fd.write(score)
+        fd.write("<tr>")
+        fd.write(fmt.format(image_url))
+        fd.write(fmt.format(name))
+        fd.write(fmt.format(eng_name))
+        fd.write(fmt.format(description))
+        fd.write(fmt.format(rating))
+        fd.write(fmt.format(genres))
+        fd.write("</tr>")
 
 
 f = open("main.html", "a")
 html_str = """
+            </table>
+            
         </center>
         
     </body>
